5cead9c1d83ec74fab23a3e971b0161e0f3a06ed
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2020-05-19 17:34:26 -0400

    tailwindcss basic setup complete, added single basic home route also

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home()
+{
+    return (
+        <div className="bg-gray-200">
+            <header className="text-5xl text-indigo-400 font-bold text-center">
+                App Name
+            </header>
+        </div>
+    )
+}
