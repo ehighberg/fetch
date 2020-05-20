4abbb7dc9dcccb3db70214dc4c6e1d73034d8a78
Errol Highberg <errol.highberg@gmail.com>
2020-05-20 18:19:47 -0400

    remove personal route

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import { Switch, Route } from 'react-router-dom'
 import './css/style.css'
 import Home from './pages/Home'
 import Nav from './components/Nav'
-import Errol from './pages/Errol'
 import Trev from './pages/Trev'
 import Tara from './pages/Tara'
 import UserProfile from './components/UserProfile'
@@ -21,7 +20,6 @@ const App = () =>
       <Switch>
         <Route exact path="/" render={(props) => (<Home {...props} />)} />
         <Route exact path="/tara" render={(props) => (<Tara {...props} />)} />
-        <Route exact path="/errol" render={(props) => (<Errol {...props} />)} />
         <Route exact path="/trev" render={(props) => (<Trev {...props} />)} />
         <Route exact path="/users" render={(props) => (<UserList {...props} />)} />
         <Route exact path="/users/:id" render={(props) => (<UserProfile {...props} />)} />
