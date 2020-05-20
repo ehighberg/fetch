60f5ee093441f019a096b5555a1744a1ba6afec8
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2020-05-20 16:38:42 -0400

    Basic post functionality working

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -8,7 +8,7 @@ export default function Post(props)
         <div className="flex border-gray-500 rounded border">
             <div className="flex bg-gray-300 w-1/4">
                 <img src={user.avatar} />
-                <div>{`${user.first_name} ${user.last_name}`}</div>
+                <div>{user.name}</div>
             </div>
             <div className="flex bg-gray-200 w-3/4">
                 {content}
