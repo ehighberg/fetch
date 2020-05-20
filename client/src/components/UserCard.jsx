4d7532600a5bb56c3188e044ab601d357bfa23c2
khimarra <tsolbrig87@gmail.com>
2020-05-20 19:00:20 -0400

    add color to presence in usercard

--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -4,37 +4,28 @@ export default function UserCard(props) {
     console.log(props.user)
 
     let user = props.user
-
     let presence = user.presence
+    let color
 
-    let showPresence = (presence) => {
-        if (presence == 'online') {
-            return (
-                <div className='rounded-full bg-green-500 w-5 h-3 m-2'></div>
-            )
-        } else if (presence == 'offline') {
-            return (
-                <div className='rounded-full bg-red-500 w-5 h-3 m-2'></div>
-            )
-        } else {
-            return (
-                <div className='rounded-full bg-yellow-500 w-5 h-3 m-2'></div>
-            )
-        }
+    if (presence == 'online') {
+        color = 'bg-green-400'
+    } else if (presence == 'offline') {
+        color = 'bg-red-600'
+    } else {
+        color = 'bg-yellow-500'
     }
     
-
-
+    
     return (
         <div className='flex flex-row m-2 bg-gray-200'>
-            {showPresence(presence)}
+            <div className={`rounded-full ${color} w-5 h-3 m-2`}></div>
             <div className='w-64'>
                 <img className='w-2\/3 rounded-full m-4' src={user.avatar}></img>
             </div>
             <div className='m-0 w-full'>
                 <div>{user.name}</div>
                 <div>{user.title}</div>
-                <div>pronouns</div>
+                <div>{user.pronouns}</div>
             </div>
         </div>
     )
