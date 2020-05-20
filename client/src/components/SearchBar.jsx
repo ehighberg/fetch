ce34869361a95dc029ba2696a5b78cf3636e8895
Errol Highberg <errol.highberg@gmail.com>
2020-05-20 19:21:02 -0400

    working form

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,34 +1,55 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Formik, Form, Field } from 'formik'
 import { useHistory } from 'react-router-dom'
 
 import { getUsersByFieldAndQuery } from '../services/APIHelper'
 
 export default function SearchBar() {
-  const [searchResults, setSearchResults] = useState([])
   const history = useHistory()
 
-  const fetchSearchResults = async (searchType, query) => {
+  const gotoSearchResults = async (values) => {
+    const { searchType, query } = values
     const searchResults = await getUsersByFieldAndQuery(searchType, query)
     console.log(searchResults)
-    setSearchResults(searchResults)
+    if (searchType === 'team') {
+      const teamId = searchResults[0].team.id
+      history.push(`/teams/${teamId}`)
+    } else {
+      history.push('/results')
+    }
   }
 
-  const availableSearchTypes = ['name', 'tag', 'team', 'bio', 'presence']
-
-  useEffect(() => {
-    fetchSearchResults('bio', 'tumblr')
-  }, [])
+  const availableSearchTypes = ['team', 'name', 'tag', 'presence', 'bio']
+  const generateOptions = () => {
+    return availableSearchTypes.map((searchType, index) => {
+      const selected = (searchType === 'team')
+      return (
+        <option
+          key={index}
+          value={searchType}
+          selected={selected}
+        >
+          {searchType}
+        </option>
+      )
+    })
+  }
 
   return (
     <Formik
       initialValues={{ searchType: 'team', query: '' }}
-      onSubmit={async () => {
-        await fetchSearchResults()
-        history.push()
-      }}
+      onSubmit={gotoSearchResults}
     >
-
+      <Form>
+        <label>Search By:</label>
+        <Field as='select' name='searchType'>
+          {generateOptions()}
+        </Field>
+        <Field type='input' name='query' placeholder='Search for...'/>
+        <button type='submit'>
+          Search
+        </button>
+      </Form>
     </Formik>
   )
 }
