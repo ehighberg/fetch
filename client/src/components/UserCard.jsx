6948831215ea8f5bdd40837c8ca716a843f5d3b8
khimarra <tsolbrig87@gmail.com>
2020-05-20 15:15:03 -0400

    merge from dev

--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -4,11 +4,13 @@ import UserProfile from './UserProfile'
 export default function UserCard(props) {
     console.log(props.user)
 
+    let user = props.user
+
     return (
         <div className='flex flex-row flex-wrap bg-gray-200 m-0 w-64'>
-            User Card
+            {user.name}
             <UserProfile
-                user={props.user}
+                user={user}
             />
         </div>
     )
