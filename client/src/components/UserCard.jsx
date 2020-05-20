e9816d94e6ca0da1f58a720f6ad7af0779e57451
khimarra <tsolbrig87@gmail.com>
2020-05-20 15:06:51 -0400

    prelim style usercard for readability

--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -5,7 +5,7 @@ export default function UserCard(props) {
     console.log(props.user)
 
     return (
-        <div>
+        <div className='flex flex-row flex-wrap bg-gray-200 m-0 w-64'>
             User Card
             <UserProfile
                 user={props.user}
--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -19,7 +19,7 @@ export default function UserList(props) {
     return (
         <div>
             <h1 className='text-xl text-purple-700'>User List</h1>
-            <div>
+            <div className='flex flex-row flex-wrap justify-around content-between'>
                 {users && users.map((user, index) => {
                     if (Object.keys(users).length === 0) {
                         return (
