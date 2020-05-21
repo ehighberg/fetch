0131742e4f26f7d2a02f6921bf306f97d2c67097
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2020-05-21 01:41:11 -0400

    basic team page

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,39 +1,65 @@
 import React, { useEffect, useState } from 'react'
 import UserCard from './UserCard'
-import { getAllUsers, getManyUsersById } from '../services/APIHelper'
+import { getAllUsers, getManyUsersById, getUserById } from '../services/APIHelper'
 import { Link } from 'react-router-dom'
 
 
-export default function UserList(props) {
+export default function UserList(props)
+{
 
     const [users, setUsers] = useState([])
+    const [manager, setManager] = useState(null)
 
-    useEffect(() => {
-      if (!props.userIds) {
-        fetchUsers()
-      } else {
-        fetchManyUsersById(props.userIds)
-      }
+    useEffect(() =>
+    {
+        if (!props.userIds)
+        {
+            fetchUsers()
+        } else
+        {
+            fetchManyUsersById(props.userIds)
+        }
+        if (props.managerId)
+        {
+            fetchManagerById(props.managerId)
+        }
     }, [])
 
-    const fetchUsers = async () => {
+    const fetchUsers = async () =>
+    {
         const res = await getAllUsers()
         setUsers(res)
     }
 
-    const fetchManyUsersById = async (userIds) => {
-      console.log(props)
-      console.log(userIds)
-      const res = await getManyUsersById(userIds)
-      setUsers(res)
+    const fetchManyUsersById = async (userIds) =>
+    {
+        console.log(props)
+        console.log(userIds)
+        const res = await getManyUsersById(userIds)
+        setUsers(res)
+    }
+
+    const fetchManagerById = async (managerId) =>
+    {
+        const res = await getUserById(managerId)
+        setManager(res)
     }
 
     return (
         <div>
             <h1 className='text-xl text-purple-700'>User List</h1>
             <div>
-                {users && users.map((user, index) => {
-                    if (Object.keys(users).length === 0) {
+                {manager && (
+                    <Link key={-1} to={`/users/${manager.id}`}>
+                        <UserCard
+                            user={manager}
+                        />
+                    </Link>
+                )}
+                {users && users.map((user, index) =>
+                {
+                    if (Object.keys(users).length === 0)
+                    {
                         return (
                             <div>
                                 Loading...
