bba46a87b6ec4d9efbcd55662e0b4afb206ead2d
khimarra <tsolbrig87@gmail.com>
2020-05-20 21:30:40 -0400

    make status component

--- client/src/components/Avatar.jsx
+++ client/src/components/Avatar.jsx
@@ -6,8 +6,8 @@ export default function Avatar(props) {
 
     return (
         <div>
-            <div className='m-4'>{user.name}</div>
-            <div>{user.title}</div>
+            <div className='text-2xl m-4'>{user.name}</div>
+            <div className='text-xl'>{user.title}</div>
             <div className='mx-auto my-10 w-64 h-64'>
                 <img className='w-full rounded-full' src={user.avatar}></img>
             </div>
--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { getUserById } from '../services/APIHelper'
 import Avatar from './Avatar'
 import Presence from './Presence'
+import Status from './Status'
 
 export default function UserProfile(props) {
 
@@ -22,12 +23,16 @@ export default function UserProfile(props) {
 
     return (
         <div className='text-center w-screen'>
-            <div className='bg-gray-200 pt-10 pb-4'>
-                Profile Details
+            <div className='bg-gray-200 pb-4'>
+                <div className='text-3xl bg-gray-500 p-6'>Profile Details</div>
                     <Avatar
                         user={user}
                     />
-                    <div className='flex flex-row justify-evenly my-6 mx-auto w-1/4'>
+                    
+                <Status
+                    user={user}
+                />
+                    {/* <div className='flex flex-row justify-evenly my-6 mx-auto w-1/4'>
                         <div className='w-5 h-5'>
                             <Presence
                                 presence={user.presence}
@@ -38,11 +43,31 @@ export default function UserProfile(props) {
                     </div>
                 <div className='border-black border-2 rounded-md mx-12 my-3 p-4'>
                     {user.status_message}
+                </div> */}
+            </div>
+
+            <div className='text-xl flex flex-row justify-between p-6 w-11/12 m-auto'>
+                <div>Contact</div>
+                <div>Prefers {user.preferred_contact_method}</div>
+            </div>
+
+            <div className='border-black border-2 rounded-md mx-12 my-3 p-4'>
+                <div className='text-left'>
+                    <div>Phone: {user.phone}</div>
+                    <div>Slack: {user.slack}</div>
+                    <div>Email: {user.email}</div>
                 </div>
             </div>
-            <div>{user.phone}</div>
-            <div>{user.slack}</div>
-            <div>{user.email}</div>
+
+            <div>Average response time: {user.average_response_time_minutes} minutes</div>
+
+            <div>Request a meeting</div>
+
+            {/* <div>
+                <div>Looking for something specific</div>
+                <div>{user.tags.name}</div>
+            </div> */}
+                
         </div>
     )
 }
