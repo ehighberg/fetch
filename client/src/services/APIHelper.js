2c6a49b2249f752a85fb1aa3449680c0f8225513
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2020-05-21 00:04:58 -0400

    v1 of realtime chat

--- client/src/services/APIHelper.js
+++ client/src/services/APIHelper.js
@@ -1,17 +1,26 @@
 import axios from 'axios'
 
 let apiUrl
+export let wsUrl
+
 const apiUrls = {
   production: 'https://mysterious-anchorage-39512.herokuapp.com/',
   development: 'http://localhost:3000/'
 }
 
+const wsUrls = {
+  production: 'wss://mysterious-anchorage-39512.herokuapp.com/cable',
+  development: 'ws://localhost:3000/cable'
+}
+
 if (window.location.hostname === 'localhost')
 {
   apiUrl = apiUrls.development
+  wsUrl = wsUrls.development
 } else
 {
   apiUrl = apiUrls.production
+  wsUrl = wsUrls.production
 }
 
 const api = axios.create({
@@ -77,43 +86,54 @@ export const getAllUsers = async () =>
   {
     const users = await api.get('/users')
     return users.data
-  } catch (error) {
+  } catch (error)
+  {
     console.error(error)
   }
 }
 
-export const getUserById = async (user_id) => {
-    try {
-        const user = await api.get(`/users/${user_id}`)
-        return user.data
-    } catch (error) {
-        console.error(error)
-    }
+export const getUserById = async (user_id) =>
+{
+  try
+  {
+    const user = await api.get(`/users/${user_id}`)
+    return user.data
+  } catch (error)
+  {
+    console.error(error)
+  }
 }
 
-export const getUsersByFieldAndQuery = async (searchType, query) => {
-  try {
+export const getUsersByFieldAndQuery = async (searchType, query) =>
+{
+  try
+  {
     const users = await getAllUsers()
 
-    switch(searchType) {
+    switch (searchType)
+    {
       case 'tag':
-        return users.data.filter(user => {
+        return users.data.filter(user =>
+        {
           const userTags = user.tags.map(tag => tag.name)
           return userTags.includes(query)
         })
 
       case 'team':
-        return users.data.filter(user => {
+        return users.data.filter(user =>
+        {
           return user.team.name.includes(query)
         })
 
       default:
-        return users.data.filter(user => {
+        return users.data.filter(user =>
+        {
           return user[searchType].includes(query)
         })
     }
 
-  } catch(error) {
+  } catch (error)
+  {
     console.error(error)
   }
 }
