63e2fcc51e54cce5a5a031391b13248ed64c355e
Errol Highberg <errol.highberg@gmail.com>
2020-05-21 00:32:31 -0400

    moved SearchBar to Home

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,29 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useHistory } from 'react-router-dom'
-
-import SearchBar from './SearchBar'
 
 
 export default function Nav(props) {
-  const [searchActive, setSearchActive] = useState(false)
   const history = useHistory()
-
 
   return (
     <nav>
-      {!searchActive &&
-        <>
-          <button onClick={() => history.goBack()}>Back</button>
-          <button onClick={() => history.push('/')}>Home</button>
-          <button onClick={() => setSearchActive(true)}>Search</button>
-        </>
-      }
-      {searchActive &&
-        <SearchBar
-          setSearchResults={props.setSearchResults}
-          setSearchActive={setSearchActive}
-        />
-      }
+        <button onClick={() => history.goBack()}>Back</button>
+        <button onClick={() => history.push('/')}>Home</button>
+        <button onClick={() => null}>HAMBURGER</button>
     </nav>
   )
 }
