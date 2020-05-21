cc2e554b906a105c1b00f7a766e593d87cf5197a
Errol Highberg <errol.highberg@gmail.com>
2020-05-21 17:56:36 -0400

    embed pictures

--- client/src/components/Pics.jsx
+++ client/src/components/Pics.jsx
@@ -4,7 +4,9 @@ export default function Pics(props) {
     return (
         <div>
             <div className='text-purple-700 text-2xl text-left p-10'>Pics</div>
-            {/* insert pics here */}
+            <img src={require('../assets/pic1.jpg')} alt='personal' />
+            <img src={require('../assets/pic2.jpg')} alt='personal' />
+            <img src={require('../assets/pic3.jpg')} alt='personal' />
         </div>
     )
 }
