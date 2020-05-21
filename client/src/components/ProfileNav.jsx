a7d62cf27b9743e9ad7a26e459ff1d52fcb32a5d
khimarra <tsolbrig87@gmail.com>
2020-05-21 15:04:12 -0400

    fix dotlink style

--- client/src/components/ProfileNav.jsx
+++ client/src/components/ProfileNav.jsx
@@ -5,13 +5,15 @@ export default function ProfileNav(props) {
 
     console.log(props.user)
 
+    let dotStyle = 'bg-transparent border rounded-full hover:bg-white w-4 h-4'
+
     return (
         <div className='flex flex-row justify-evenly w-1/4 m-auto mt-4'>
-            <NavLink activeClassName="bg-purple-700" className='bg-gray-400 rounded-full hover:bg-white w-5 h-5' exact to={`/users/${props.user.id}`}>
+            <NavLink activeClassName="bg-purple-100" className={`${dotStyle}`} exact to={`/users/${props.user.id}`}>
             </NavLink>
-            <NavLink activeClassName="bg-purple-700" className='bg-gray-400 rounded-full hover:bg-white w-5 h-5 active:bg-gray-600' exact to={`/users/${props.user.id}/page2`}>
+            <NavLink activeClassName="bg-purple-100" className={`${dotStyle}`} exact to={`/users/${props.user.id}/page2`}>
             </NavLink>
-            <NavLink activeClassName="bg-purple-700" className='bg-gray-400 rounded-full hover:bg-white w-5 h-5 active:bg-gray-600' exact to={`/users/${props.user.id}/page3`}>
+            <NavLink activeClassName="bg-purple-100" className={`${dotStyle}`} exact to={`/users/${props.user.id}/page3`}>
             </NavLink>
         </div>
     )
