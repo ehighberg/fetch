38bd1148073371c958f450ba801084091342a3da
khimarra <tsolbrig87@gmail.com>
2020-05-21 16:32:12 -0400

    add box shadows

--- client/src/pages/UserProfilePage3.jsx
+++ client/src/pages/UserProfilePage3.jsx
@@ -30,7 +30,7 @@ export default function UserProfilePage3(props) {
     return (
         <div {...handlers} className='text-center w-screen'>
             <div className='text-3xl text-purple-700 p-6 font-bold'>PROFILE DETAILS</div>
-            <div className='purple-gradient py-4'>
+            <div className='purple-gradient py-4 shadow-xl'>
                 <ProfileNav user={user} />
                 <Avatar user={user} />
             </div>
