c4daff904ad185a35243d49f4ba2d0c70cd9c257
Errol Highberg <errol.highberg@gmail.com>
2020-05-21 18:28:18 -0400

    add contact icons

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -9,12 +9,23 @@ export default function Contact(props) {
             <div className='font-bold text-purple-700 text-lg'>Contact</div>
             <div className='border-b-2 mx-12 my-3 p-4'>
                 {/* need contact message? */}
-                <div className='font-bold text-purple-700 text-lg mb-6'>Prefers {user.preferred_contact_method}</div>
+                <div className='font-bold text-purple-700 text-lg mb-6'>Prefers Chat</div>
+                <img src={require('../assets/Icons/Chat.png')} alt='contact chat' />
                 <div className='font-bold text-purple-700 text-lg mb-6'>Average response time: {user.average_response_time_minutes} minutes</div>
-                <div>Phone: {user.phone}</div>
-                <div>Chat: (room)</div>
-                <div>Slack: {user.slack}</div>
-                <div>Email: {user.email}</div>
+                <div className='flex'>
+                  <div>
+                    <img src={require('../assets/Icons/Phone.png')} alt={`Phone: ${user.phone}`} />
+                  </div>
+                  <div>
+                    <img src={require('../assets/Icons/Chat.png')} alt={`Chat: (room)`} />
+                  </div>
+                  <div>
+                    <img src={require('../assets/Icons/Video.png')} alt={`Slack: ${user.slack}`} />
+                  </div>
+                  <div>
+                    <img src={require('../assets/Icons/Video.png')} alt={`Email: ${user.email}`} />
+                  </div>
+                </div>
             </div>
 
             <div className='border-b-2 mx-12 my-3 p-4'>
