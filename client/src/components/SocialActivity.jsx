bb275d4341426dce00681f8b95bf9686a4b030f9
khimarra <tsolbrig87@gmail.com>
2020-05-21 18:47:50 -0400

    added gray circles to interests

--- client/src/components/SocialActivity.jsx
+++ client/src/components/SocialActivity.jsx
@@ -4,7 +4,7 @@ export default function SocialActivity(props) {
     return (
         <div>
             <div className='font-bold text-purple-700 text-lg mt-10'>Recent Social Activity</div>
-            <div className='mx-12 p-4'>
+            <div className='w-4/5 h-64 mx-auto my-4 rounded-lg bg-gray-300'>
                 content...
             </div>
         </div>
