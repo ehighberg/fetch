329e04cfde7e966442b54558e54fda5b8ae996ce
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2020-05-21 17:15:27 -0400

    working on styling homepage, created redirects file, created login page

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -4,24 +4,30 @@ import { useHistory } from 'react-router-dom'
 
 import { getUsersByFieldAndQuery } from '../services/APIHelper'
 
-export default function SearchBar(props) {
+export default function SearchBar(props)
+{
   const history = useHistory()
 
-  const gotoSearchResults = async (values) => {
+  const gotoSearchResults = async (values) =>
+  {
     const { searchType, query } = values
     const searchResults = await getUsersByFieldAndQuery(searchType, query)
-    if (searchType === 'team') {
+    if (searchType === 'team')
+    {
       const teamId = searchResults[0].team.id
       history.push(`/teams/${teamId}`)
-    } else {
+    } else
+    {
       props.setSearchResults(searchResults)
       history.push('/results')
     }
   }
 
   const availableSearchTypes = ['Team', 'Bio', 'Name', 'Presence', 'Tag', 'Title']
-  const generateOptions = () => {
-    return availableSearchTypes.map((searchType, index) => {
+  const generateOptions = () =>
+  {
+    return availableSearchTypes.map((searchType, index) =>
+    {
       return (
         <option key={index} value={searchType}>
           {searchType}
@@ -40,18 +46,18 @@ export default function SearchBar(props) {
         <Field as='select' name='searchType'>
           {generateOptions()}
         </Field>
-        <div className='flex flex-row justify-between bg-white border-2 border-purple-700 rounded-full w-3/4 p-4 mx-auto my-10'>
-            <Field
-                type='input'
-                name='query'
-                placeholder='Alex; Engineering; Updates'
-                className=''
-            />
-            <button
-                type='submit'
-                className='cursor-pointer inline-block text-xl'
-            >
-                ICON
+        <div className='flex flex-row justify-between bg-white border-2 border-yellow-400 rounded-full w-3/4 p-4 mx-auto my-10'>
+          <Field
+            type='input'
+            name='query'
+            placeholder='Alex; Engineering; Updates'
+            className=''
+          />
+          <button
+            type='submit'
+            className='cursor-pointer inline-block text-xl'
+          >
+            ICON
             </button>
         </div>
       </Form>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,58 +7,66 @@ import SearchBar from '../components/SearchBar'
 
 export default function Home(props)
 {
-    const history = useHistory()
+  const history = useHistory()
 
-    return (
-        <div className="text-center bg-gray-100">
-            <div className="bg-purple-700 text-5xl text-yellow-400 font-bold text-center">
-                Fetch
-            </div>
+  return (
+    <div className="flex flex-col text-center bg-gray-100">
+      <div className=" font-poppins my-6 text-purple-700 text-2xl uppercase">
+        Home
+      </div>
 
-            {!localStorage.getItem('userId') && (
-              <Formik
-                initialValues={{ email: '', password: '' }}
-                onSubmit={ async (values, actions) => {
-                  const userId = await getUserByEmail(values.email)
-                  if (userId) {
-                    localStorage.setItem('userId', userId)
-                    history.push('/')
-                  } else {
-                    actions.resetForm()
-                  }
-                }}
-              >
-                <Form id='login-container' className="flex flex-col mt-32">
-                  {/* <label>Email</label> */}
-                  <Field type='email' name='email' placeholder="email@domain.com" className='bg-white border-2 border-purple-700 rounded-full w-3/4 p-4 mx-auto' />
-                  {/* <label>Password</label> */}
-                  <Field type='password' name='password' placeholder="password" className='bg-white border-2 border-purple-700 rounded-full w-3/4 p-4 mx-auto mt-8' />
-                  <button type='submit' className='bg-purple-700 text-white border-2 border-purple-700 rounded-full w-1/4 p-2 mx-auto mt-8'>Login</button>
-                </Form>
-              </Formik>
-            )}
+      <div className="bg-purple-700 w-32 h-32">
 
-            {localStorage.getItem('userId') && (
-              <div>
-                <SearchBar
-                  setSearchResults={props.setSearchResults}
-                />
-                <div className='text-4xl text-purple-700'>Company Directory</div>
-                <Link to='/teams' className='flex flex-row w-3/5 text-3xl mx-auto justify-between border-b-2 my-4'>
-                  <div>Teams</div>
-                  <div>ICON</div>
-                </Link>
-                <Link to='/users' className='flex flex-row w-3/5 text-3xl mx-auto justify-between border-b-2 my-4'>
-                  <div>People</div>
-                  <div>ICON</div>
-                </Link>
-              </div>
-            )}
+      </div>
 
-            <button onClick={() => {
-                localStorage.removeItem('userId')
-                history.push('/')
-              }} className='bg-purple-700 text-white border-2 border-purple-700 rounded-full w-1/4 p-2 mx-auto mt-8'>Logout</button>
+      {!localStorage.getItem('userId') && (
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={async (values, actions) =>
+          {
+            const userId = await getUserByEmail(values.email)
+            if (userId)
+            {
+              localStorage.setItem('userId', userId)
+              history.push('/')
+            } else
+            {
+              actions.resetForm()
+            }
+          }}
+        >
+          <Form id='login-container' className="flex flex-col mt-32">
+            {/* <label>Email</label> */}
+            <Field type='email' name='email' placeholder="email@domain.com" className='bg-white border-2 border-purple-700 rounded-full w-3/4 p-4 mx-auto' />
+            {/* <label>Password</label> */}
+            <Field type='password' name='password' placeholder="password" className='bg-white border-2 border-purple-700 rounded-full w-3/4 p-4 mx-auto mt-8' />
+            <button type='submit' className='bg-purple-700 text-white border-2 border-purple-700 rounded-full w-1/4 p-2 mx-auto mt-8'>Login</button>
+          </Form>
+        </Formik>
+      )}
+
+      {localStorage.getItem('userId') && (
+        <div>
+          <SearchBar
+            setSearchResults={props.setSearchResults}
+          />
+          <div className='text-4xl text-purple-700'>Company Directory</div>
+          <Link to='/teams' className='flex flex-row w-3/5 text-3xl mx-auto justify-between border-b-2 my-4'>
+            <div>Teams</div>
+            <div>ICON</div>
+          </Link>
+          <Link to='/users' className='flex flex-row w-3/5 text-3xl mx-auto justify-between border-b-2 my-4'>
+            <div>People</div>
+            <div>ICON</div>
+          </Link>
         </div>
-    )
+      )}
+
+      <button onClick={() =>
+      {
+        localStorage.removeItem('userId')
+        history.push('/')
+      }} className='bg-purple-700 text-white border-2 border-purple-700 rounded-full w-1/4 p-2 mx-auto mt-8'>Logout</button>
+    </div>
+  )
 }
