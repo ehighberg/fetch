7150c053a84a56536da7ca43602a56d2ac740b63
khimarra <tsolbrig87@gmail.com>
2020-05-20 23:56:43 -0400

    made ProfileNav and added avatar to other profile pages

---
+++ client/src/components/ProfileNav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ProfileNav(props) {
+
+    return (
+        <>
+            0 0 0 0
+        </>
+    )
+}
--- client/src/pages/UserProfilePage2.jsx
+++ client/src/pages/UserProfilePage2.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProfileNav from '../components/ProfileNav'
+import Avatar from '../components/Avatar'
+import { getUserById } from '../services/APIHelper'
 
-export default function UserProfilePage2()
-{
+export default function UserProfilePage2(props) {
+
+    console.log(props)
+
+    const userId = props.match.params.id
+
+    const [user, setUser] = useState([])
+
+    useEffect(() => {
+        fetchUser()
+    }, [])
+
+    const fetchUser = async () => {
+        const res = await getUserById(userId)
+        setUser(res)
+    }
+
     return (
         <div>
+            <ProfileNav />
+            <Avatar
+                user={user}
+            />
             UserProfilePage2
         </div>
     )
