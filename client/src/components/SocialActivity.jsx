b50cfccf56c799f03bd6b1f04c7d134cdeeff7da
Errol Highberg <errol.highberg@gmail.com>
2020-05-21 17:42:05 -0400

    embed social media images

--- client/src/components/SocialActivity.jsx
+++ client/src/components/SocialActivity.jsx
@@ -4,7 +4,10 @@ export default function SocialActivity(props) {
     return (
         <div>
             <div className='text-purple-700 text-2xl p-10'>Recent Social Activity</div>
-            {/* insert social activity here */}
+            <div className=''>
+              <img src={require('../assets/social1.png')} alt='social media post' />
+              <img src={require('../assets/social2.png')} alt='social media post' />
+            </div>
         </div>
     )
 }
