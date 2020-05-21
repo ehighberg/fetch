4ff5b1e8dd9723009bddd9a3d8c4a5e13f749907
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2020-05-21 10:24:10 -0400

    added provided colors to taiwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,24 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          ...colors.purple,
+          '700': '#632ca6',
+        },
+        yellow: {
+          ...colors.yellow,
+          '400': '#f6bb50',
+        },
+        gray: {
+          ...colors.gray,
+          '100': '#f6bb50',
+        }
+      }
+    }
   },
   variants: {
     backgroundColor: ['responsive', 'hover', 'focus', 'active'],
