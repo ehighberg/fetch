0c0be35b8db6dcdbdda94caa8e5c56215090130c
khimarra <tsolbrig87@gmail.com>
2020-05-21 20:16:01 -0400

    finished dots component

--- client/src/components/DotLink.jsx
+++ client/src/components/DotLink.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function DotLink() {
     return (
-        <div className='flex flex-row w-8'>
-            <div className='border border-purple-700 rounded-full m-1 bg-gray-400'></div>
-            <div className='border border-purple-700 rounded-full m-1 bg-gray-400'></div>
-            <div className='border border-purple-700 rounded-full m-1 bg-gray-400'></div>
+        <div className='flex flex-row w-auto middle m-4'>
+            <div className='border border-purple-700 dots bg-gray-400'></div>
+            <div className='border border-purple-700 dots bg-gray-400'></div>
+            <div className='border border-purple-700 dots bg-gray-400'></div>
         </div>
     )
 }
