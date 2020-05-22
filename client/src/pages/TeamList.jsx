009f725b46aaefe56681d9119eb633085a3eed23
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2020-05-22 11:44:58 -0400

    added added images to team and teams pages

--- client/src/pages/TeamList.jsx
+++ client/src/pages/TeamList.jsx
@@ -5,6 +5,9 @@ import { getTeams } from '../services/APIHelper'
 import UserList from '../components/UserList'
 import DotLink from '../components/DotLink'
 
+import DOGBG from '../assets/Images/Department dog background small.png'
+import DOG from '../assets/Images/DOG FETCH 2.gif'
+
 export default function TeamList()
 {
   const [teamList, setTeamList] = useState([])
@@ -23,13 +26,26 @@ export default function TeamList()
   return (
     <div>
       <div className="font-poppins text-2xl text-center text-purple-700 uppercase py-6">Teams</div>
+
+      <div className="flex justify-center">
+        <div className="flex h-56 justify-center w-full" style={{
+          backgroundImage: `url(${DOGBG})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "267px 174px",
+
+        }}>
+          <img className=" h-48 object-cover" src={DOG}></img>
+        </div>
+      </div>
+
       <div id='team-list'>
         {teamList && (
           teamList.map((team) =>
           {
             const teamId = team.id
             return (
-              <Link to={`/teams/${teamId}`} key={teamId} className='flex flex-row w-3/5 text-2xl mx-auto justify-between border-b-2 my-4'>
+              <Link to={`/teams/${teamId}`} key={teamId} className='flex flex-row w-4/5 text-2xl mx-auto justify-between border-b-2 my-4'>
                 <p>{team.name}</p>
                 <DotLink />
               </Link>
